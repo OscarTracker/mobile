acb27fca50b82696811bfeecdc8829c2baaa0f47
rizzotto <guilherme.rizzotto@acad.pucrs.br>
2022-02-13 17:42:01 -0300

    search component

---
+++ src/components/Search/index.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { SearchBar } from 'react-native-elements'
+import PropTypes from 'prop-types'
+
+export default function Search({ onSearch, placeholder, ...rest }) {
+  const [search, setSearch] = useState('')
+
+  const updateSearch = (search) => {
+    setSearch(search)
+    onSearch(search)
+  }
+
+  return (
+    <View style={styles.view}>
+      <SearchBar
+        containerStyle={{
+          backgroundColor: '#212121',
+          padding: 0,
+          borderBottomColor: 'transparent',
+          borderTopColor: 'transparent',
+          paddingHorizontal: 20, // change here
+        }}
+        onChangeText={updateSearch}
+        placeholder={placeholder}
+        round
+        value={search}
+        {...rest}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  view: {
+    width: '100%',
+  },
+})
+
+Search.propTypes = {
+  onSearch: PropTypes.func,
+  placeholder: PropTypes.string,
+}
+
+Search.defaultProps = {
+  onSearch: undefined,
+  placeholder: undefined,
+}
